2a9aed5b38ed9e721c8cb4273d04a5f420d03b02
glitterash <ashishhsharma1990@gmail.com>
2015-12-21 14:49:07 +0530

    home page functionailty

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,30 @@
 angular.module('starterControllers', [])
 
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl',['$scope',function($scope) {
     console.log("Controller Called");
-   	
+		$scope.one = true; 
+		$scope.two = false; 
+		$scope.three = false;
+
+		$scope.showOne = function () {
+			console.log("sdfd")
+			$scope.one = true; 
+			$scope.two = false; 
+			$scope.three = false;
+		}
+
+		$scope.showTwo = function() {
+			console.log("two function")
+			$scope.one = false; 
+			$scope.two = true; 
+			$scope.three = false;
+		}
+
+		$scope.showThree = function() {
+			$scope.one = false; 
+			$scope.two = false; 
+			$scope.three = true;
+		}   	
 }]);
 
 
